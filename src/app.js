31a612f9bef0b409c7c919ae23bb65b26b701da3
teacher-cheater <johnnykayfun@gmail.com>
2024-07-15 22:20:23 +0300

    add: ts, webpack plugin

--- src/app.js
+++
@@ -1,3 +0,0 @@
-export function showMsg(str){
-    return str;
-}
---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+
+type Mode = "production" | "development";
+
+interface EnvVariables {
+  mode: Mode;
+}
+
+export default (env: EnvVariables) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+  };
+  return config;
+};
