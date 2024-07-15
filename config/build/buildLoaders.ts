6f97e8f0a531b8ed0c3394d59e1c62787bfe646f
teacher-cheater <johnnykayfun@gmail.com>
2024-07-16 00:34:04 +0300

    add min css, file decomposition

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,22 @@
+import { ModuleOptions } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+
+export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
+  const isDev = options.mode === "development";
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      "css-loader",
+      "sass-loader",
+    ],
+  };
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [scssLoader, tsLoader];
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,52 +1,23 @@
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { buildWebpack } from "./config/build/buildWebpack";
 import path from "path";
-
-type Mode = "production" | "development";
+import { BuildMode, BuildPaths } from "./config/build/types/types";
 
 interface EnvVariables {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
 }
 
 export default (env: EnvVariables) => {
-  const isDev = env.mode === "development";
-
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, "build"),
     entry: path.resolve(__dirname, "src", "index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }),
-    ].filter(Boolean),
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devtool: isDev ? "inline-source-map" : false,
-    devServer: isDev && {
-      port: env.port ?? 3000,
-      open: true,
-    },
+    html: path.resolve(__dirname, "public", "index.html"),
   };
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths,
+  });
   return config;
 };
